1ae91b571d1999c3b8f5500c7d903a5029a70d07
quaqiuheike <1831814662@qq.com>
2019-12-12 16:43:51 +0800

    提交所练习的demo

---
+++ jd练习/练习demo/js/index.js
@@ -0,0 +1,9 @@
+// <!-- Initialize Swiper -->
+window.addEventListener('load', function() {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+    });
+});
